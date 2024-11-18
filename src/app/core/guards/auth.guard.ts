e3c23639be6d3237ac1745a9294f6599d757bdfd
Michael Lima <michafloyd@gmail.com>
2024-11-17 23:41:33 -0300

    alterações

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { CanActivateFn } from '@angular/router';
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const authGuard: CanActivateFn = (route, state) => {
   return true;
 };
--- src/app/core/services/user/user.service.ts
+++ src/app/core/services/user/user.service.ts
@@ -4,6 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserService {
-
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor() { }
 }
